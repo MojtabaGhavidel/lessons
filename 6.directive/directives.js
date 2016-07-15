4c944523769cec15faa362bb81da00ae24bfd1a0
MojtabaGhavidel <mojtabaghavidel@gmail.com>
2016-07-15 18:12:13 +0430

    Lesson 6

--- 6.directive/directives.js
+++ 6.directive/directives.js
@@ -3,5 +3,18 @@ app.directive("ghBusiness", function() {
     template: "<h1>Business Page</h1>"
   };
 });
-
-
+app.directive('ghTooltip',  function(){
+    return { 
+        link: function(scope, element, attributes){
+            $(element).attr('title', attributes.ghTooltip + ' !!!' );
+            $(element).attr('data-toggle','tooltip');
+            $(element).attr('data-placement','right');
+            //reminder //console.log(attributes.anotherParam);
+		}
+	};
+});   
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip(); 
+}); 
+                                 
+            
